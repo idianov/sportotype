4e7b8a6e2c148430d8d1e68a978ab77c8df73853
idianov <dianovmail@gamil.com>
2014-11-20 21:58:58 +0300

    beggining

--- sportotype/sportotype/urls.py
+++ sportotype/sportotype/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from sport import views
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'sportotype.views.home', name='home'),
+    url(r'^$', 'sport.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
